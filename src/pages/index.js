830b7dda85b288b64d6c0102399321ea7f78d4bb
santuan <smrcouto88@gmail.com>
2020-08-12 20:14:39 -0300

    home hero image

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,16 @@ import React from "react"
 import "react-awesome-slider/dist/styles.css"
 import Layout from "../components/layout"
 import HeroWave from "../components/HeroWave"
+import AwesomeSlider from "react-awesome-slider"
 import { FiChevronRight } from "react-icons/fi"
 import { Helmet } from "react-helmet"
 import "../components/VideoReact.css"
-
+import tw from "twin.macro"
+import styled from "@emotion/styled"
+import HomeHeroImage from "../images/bg-home.jpg"
 import { AwesomeButton } from "react-awesome-button"
 import "../components/AwsBtn.css"
+import Fade from "react-reveal/Fade"
 
 import { navigate } from "gatsby" // highlight-line
 
@@ -18,52 +22,86 @@ export default () => {
         <body className="home" />
       </Helmet>
 
-      <HeroWave
-        heading="Kallpa Lúdica"
-        pattern="bg-orange-600 text-orange-500"
-        svg="M0,160L80,176C160,192,320,224,480,224C640,224,800,192,960,192C1120,192,1280,224,1360,240L1440,256L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"
-      />
-      <div className="pt-12 pb-24 bg-white">
-        <div className="flex flex-col items-center justify-center max-w-3xl p-3 py-2 mx-auto">
-          <AwesomeButton
-            action={() => {
-              navigate(`/juegos/`)
-            }}
-            type="primary"
-          >
-            Conocé la Tiendita de juegos
-            <FiChevronRight className="inline-block mt-1 ml-3" />
-          </AwesomeButton>
-        </div>
-      </div>
-      {/*
-      <AwesomeSlider>
-        <div>
-          <img
-            alt="Kallpa Lúdica"
-            src="https://images.unsplash.com/photo-1577897113292-3b95936e5206?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=681&q=80"
-          />
-        </div>
-        <div>
-          <img
-            alt="Kallpa Lúdica"
-            src="https://images.unsplash.com/photo-1582921017967-79d1cb6702ee?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80"
-          />
-        </div>
-        <div>
-          <img
-            alt="Kallpa Lúdica"
-            src="https://images.unsplash.com/photo-1519744346361-7a029b427a59?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80"
-          />
-        </div>
-        <div>
-          <img
-            alt="Kallpa Lúdica"
-            src="https://images.unsplash.com/photo-1563941406054-949225931d52?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80"
-          />
-        </div>
-      </AwesomeSlider>
-      */}
+      <Hero className="relative overflow-hidden text-indigo-500 bg-indigo-600 pattern-cross-dots-md">
+        <HeroContent>
+          <Fade bottom>
+            <Title>
+              Hola! 👋 <br /> Te damos la bienvenida a nuestra tiendita
+            </Title>
+          </Fade>
+          <div className="pt-12">
+            <div className="flex flex-col items-center justify-start max-w-3xl p-3 py-2 mx-auto">
+              <Fade bottom delay={500}>
+                <AwesomeButton
+                  action={() => {
+                    navigate(`/juegos/`)
+                  }}
+                  type="secondary"
+                >
+                  Conocé los juegos
+                  <FiChevronRight className="inline-block mt-1 ml-3" />
+                </AwesomeButton>
+              </Fade>
+            </div>
+          </div>
+        </HeroContent>
+        <img
+          src={HomeHeroImage}
+          title="bienvenides"
+          className="absolute top-0 w-auto h-screen opacity-25 sm:h-auto sm:w-full"
+        />
+        <Wave>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+            <defs xmlns="http://www.w3.org/2000/svg">
+              <filter id="dropshadow" height="130%">
+                <feGaussianBlur in="SourceAlpha" stdDeviation="3" />
+                <feOffset dx="2" dy="2" result="offsetblur" />
+                <feComponentTransfer>
+                  <feFuncA type="linear" slope="0.2" />
+                </feComponentTransfer>
+                <feMerge>
+                  <feMergeNode />
+                  <feMergeNode in="SourceGraphic" />
+                </feMerge>
+              </filter>
+            </defs>
+            <path
+              fillOpacity="1"
+              filter="url(#dropshadow)"
+              className="text-white fill-current "
+              d="M0,160L80,176C160,192,320,224,480,224C640,224,800,192,960,192C1120,192,1280,224,1360,240L1440,256L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"
+            ></path>
+          </svg>
+        </Wave>
+      </Hero>
     </Layout>
   )
 }
+
+const HeroContent = styled.div`
+  ${tw`relative z-50 flex flex-col w-full max-w-6xl pt-12 mx-auto text-left md:pb-24`}
+`
+
+const Title = styled.h1`
+  ${tw`max-w-xl mx-auto font-sans text-2xl font-bold text-center text-white md:text-4xl`}
+`
+
+const Hero = styled.div`
+  ${tw`relative flex flex-col items-start justify-center w-full pt-32 pb-32 mx-auto`}
+`
+
+const Wave = styled.div`
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  overflow: hidden;
+  line-height: 0;
+
+  .svg {
+    position: relative;
+    display: block;
+    width: calc(140% + 1.3px);
+    height: 208px;
+  }
+`
