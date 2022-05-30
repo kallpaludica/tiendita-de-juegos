19c5a673e6cc0fe6f1e1f19ea42766adcfa63d32
stn <smrcouto88@gmail.com>
2022-05-29 23:02:10 -0300

    warning remove transformer remark

--- gatsby-config.js
+++ gatsby-config.js
@@ -173,13 +173,7 @@ module.exports = {
     {
       resolve: `gatsby-transformer-remark`,
       options: {
-        // CommonMark mode (default: true)
-        commonmark: true,
-        // Footnotes mode (default: true)
         footnotes: true,
-        // Pedantic mode (default: true)
-        pedantic: true,
-        // GitHub Flavored Markdown mode (default: true)
         gfm: true,
       },
     },
